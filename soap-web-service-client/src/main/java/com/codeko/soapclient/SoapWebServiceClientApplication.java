095638d84a24487831a58ee8ae58a0ebca2fd5bf
Cuong Ngo <cuong.ngo@ascendcorp.com>
2018-03-26 11:53:18 +0700

    add files

---
+++ soap-web-service-client/src/main/java/com/codeko/soapclient/SoapWebServiceClientApplication.java
@@ -0,0 +1,33 @@
+package com.codeko.soapclient;
+
+import com.codeko.soapclient.schemas.school.StudentDetailsRequest;
+import com.codeko.soapclient.schemas.school.StudentDetailsResponse;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SoapWebServiceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWebServiceClientApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner lookup(SOAPConnector soapConnector) {
+		return args -> {
+			String name = "Sajal";//Default Name
+			if(args.length>0){
+				name = args[0];
+			}
+			StudentDetailsRequest request = new StudentDetailsRequest();
+			request.setName(name);
+			StudentDetailsResponse response =(StudentDetailsResponse) soapConnector.callWebService("http://localhost:8080/service/student-details", request);
+			System.out.println("Got Response As below ========= : ");
+			System.out.println("Name : "+response.getStudent().getName());
+			System.out.println("Standard : "+response.getStudent().getStandard());
+			System.out.println("Address : "+response.getStudent().getAddress());
+		};
+	}
+}
---
+++ soap-web-service-client/src/test/java/com/codeko/soapclient/SoapWebServiceClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeko.soapclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoapWebServiceClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
